80111ccd6420a21b18eb9b859ea9a20cc5b4edd4
Quin <quinlan.adney@gmail.com>
2014-07-16 10:02:27 -0500

    Add attr_accessor to Topping.

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,16 @@
+#We'll build a system for recording a pizza's toppings, and 
+#reporting back whether or not the pizza is vegetarian based 
+#on those toppings that were added. For the moment, we'll only
+#build the underlying code - we won't worry about building a 
+#user interface for it quite yet.
+
 class Pizza
 end
 
 class Topping
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
